b0664258a110271b1a80246317de8d8555e59d82
jcohen <jcohen@student.42.fr>
2025-02-03 14:30:01 +0100

    cpp00-ex00 implemented

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 14:08:02 by jcohen            #+#    #+#             */
+/*   Updated: 2025/02/03 14:28:48 by jcohen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)toupper(argv[i][j]);
+        std::cout << std::endl;
+    }
+    return (0);
+}
