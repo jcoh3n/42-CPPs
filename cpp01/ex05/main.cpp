227cdee5c53c61316f74557ea172edd5390bb1b5
Joey Cohen <jcohen@paul-f3Ar2s2.clusters.42paris.fr>
2025-02-20 15:50:23 +0100

    fix ctrl+D input

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 16:01:38 by j                 #+#    #+#             */
-/*   Updated: 2025/02/19 13:09:10 by jcohen           ###   ########.fr       */
+/*   Updated: 2025/02/20 15:49:24 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int main()
     while (1)
     {
         std::cout << "Enter a level (DEBUG, INFO, WARNING, ERROR) or Q to quit: ";
-        std::cin >> level;
+        if (!(std::cin >> level))
+        {
+            std::cout << "\nEOF detected" << std::endl;
+            break;
+        }
         if (level != "DEBUG" && level != "INFO" && level != "WARNING" && level != "ERROR" && level != "Q")
         {
             std::cout << "Invalid input" << std::endl;
