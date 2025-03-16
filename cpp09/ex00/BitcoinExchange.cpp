b2fb147a7a9ee332c9a629c10d47a2393ff87436
jcohen <jcohen@student.42.fr>
2025-03-16 18:27:41 +0100

    gere mieux les dates

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -32,8 +32,7 @@ void BitcoinExchange::loadDatabase(const std::string& databaseFile)
     }
 }
 
-bool BitcoinExchange::isValidDate(const std::string& date) const
-{
+bool BitcoinExchange::isValidDate(const std::string& date) const {
     if (date.length() != 10 || date[4] != '-' || date[7] != '-')
         return false;
 
@@ -43,6 +42,17 @@ bool BitcoinExchange::isValidDate(const std::string& date) const
 
     if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
         return false;
+
+    // Vérification des mois avec 30 jours
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+        return false;
+
+    // Vérification de février
+    if (month == 2) {
+        bool isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        if (day > 29 || (day == 29 && !isLeapYear))
+            return false;
+    }
 
     return true;
 }
